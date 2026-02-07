Blacksuan19/android_device_xiaomi_vince

msm8953-common: Build LiveDisplay HAL
Change-Id: I4bfac85c64599ced45983a86d98cfd810a97db99

msm8953.mk
@@ -237,7 +237,7 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay native
 PRODUCT_PACKAGES += \
-    libjni_livedisplay
+    vendor.lineage.livedisplay@1.0-service-qdcm
 
 # Media
 PRODUCT_COPY_FILES += \

EunoiaOS-Devices/eunoia_device_xiaomi_mithorium-common

msm8953.mk
@@ -237,7 +237,7 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay native
 PRODUCT_PACKAGES += \
-    libjni_livedisplay
+    vendor.lineage.livedisplay@1.0-service-sdm
 
 # Media
 PRODUCT_COPY_FILES += \