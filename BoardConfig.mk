LineageOS/android_device_xiaomi_mido

mido: lineagehw: Apply LineageOS rebrand
 * Adapt to the new Lineage SDK and temporarily disable the import
   of currently non-existing Lineage common classes.

 * Drop never required BOARD_USES_CYANOGEN_HARDWARE flag
   (only required to opt-in for common light HALs).

Change-Id: I32027a98d89c205360ca3bd09f3a5b6a57b77d34

BoardConfig.mk
@@ -108,13 +108,6 @@ TARGET_TS_MAKEUP := true
 BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
 
-# CMHW
-BOARD_USES_CYANOGEN_HARDWARE := true
-BOARD_HARDWARE_CLASS += \
-    hardware/cyanogen/cmhw \
-    $(DEVICE_PATH)/cmhw
-TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/enable_dt2w"
-
 # CNE / DPM
 BOARD_USES_QCNE := true
 
@@ -182,6 +175,10 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_mido
 # Keymaster
 TARGET_PROVIDES_KEYMASTER := true
 
+# Lineage Hardware
+BOARD_HARDWARE_CLASS += \
+    $(DEVICE__PATH)/lineagehw
+
 # Media
 TARGET_USES_MEDIA_EXTENSIONS := true
 
@@ -199,6 +196,7 @@ TARGET_PER_MGR_ENABLED := true
 
 # Power
 TARGET_POWERHAL_VARIANT := qcom
+TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/enable_dt2w"
 
 # Properties
 TARGET_SYSTEM_PROP := $(DEVICE_PATH)/system.prop

EunoiaOS-Devices/eunoia_device_xiaomi_mithorium-common

BoardConfig.mk
@@ -108,13 +108,6 @@ TARGET_TS_MAKEUP := true
 BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
 
-# CMHW
-BOARD_USES_CYANOGEN_HARDWARE := true
-BOARD_HARDWARE_CLASS += \
-    hardware/cyanogen/cmhw \
-    $(DEVICE_PATH)/cmhw
-TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/enable_dt2w"
-
 # CNE / DPM
 BOARD_USES_QCNE := true
 
@@ -186,6 +179,10 @@ TARGET_PROVIDES_KEYMASTER := true
 BOARD_LIGHTS_VARIANT := aw2013
 TARGET_PROVIDES_LIBLIGHT := true
 
+# Lineage Hardware
+BOARD_HARDWARE_CLASS += \
+    $(DEVICE_PATH)/lineagehw
+
 # Media
 TARGET_USES_MEDIA_EXTENSIONS := true
 
@@ -203,6 +200,7 @@ TARGET_PER_MGR_ENABLED := true
 
 # Power
 TARGET_HAS_NO_WIFI_STATS := true
+TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/enable_dt2w"
 
 # Properties
 TARGET_SYSTEM_PROP := $(DEVICE_PATH)/system.prop